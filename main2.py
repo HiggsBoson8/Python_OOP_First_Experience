10f8c838f1f167efeb0122677ea3cf5162e44f35
Abay <120572718+HiggsBoson8@users.noreply.github.com>
2023-02-03 00:56:02 +0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,11 +3,14 @@
 # print(a.capitalize()) # метод строки
 # print(type(a)) # class string - класс стринг имеет свои методы внутри
 
+
 import os 
 import time 
 from time import sleep
 
 os.system("clear") 
+
+
 
 # название класса должно быть с большой буквы
 class Cat:
@@ -15,8 +18,9 @@ class Cat:
     Для чего этот класс!
     """
 
+
     # def init - initialize method
-    def __init__(self, name, age): # классы не сильно отличаются от функции, все атрибуты нужно передать в init
+    def __init__(self, name, age, breed): # классы не сильно отличаются от функции, все атрибуты нужно передать в init
         # self - маршрутизатор, ссылается
         self.name = name 
         self.age = age 
@@ -38,20 +42,41 @@ class Cat:
     def __str__(self):
         return f"Вы не указали что вернуть"
 
-cat1 = Cat("Муси", 2) # экземпляр класса
-cat2 = Cat("Герман", 3) # экземпляр класса
+    
+cat1 = Cat("Муси", 2, "оранжевая кошка") # экземпляр класса
+cat2 = Cat("Никс", 11, "белый котик") # экземпляр класса
+cat3 = Cat("Кеша", 5, "черный котик")
+cat4 = Cat("Шушуина", 3, "коричневая кошка")
+cat5 = Cat("Монк", 10, "мейн-кун")
+
 
 # print(cat1.name, cat1.age, cat1.meow()) 
 # print(cat1.meow())
 
-print(cat2.meow())
+# print(cat1.meow())
+# time.sleep(3)
+# print(cat1.eat())
+# time.sleep(3)
+# print(cat1.play())
+# time.sleep(3)
+# print(cat1.sleep())
+
+
+print(cat1.eat())
+time.sleep(3)
+print(cat2.eat()) 
+time.sleep(3)
+print(cat3.eat()) 
+time.sleep(3)
+print(cat4.eat()) 
+time.sleep(3)
+print(cat5.eat()) 
 time.sleep(3)
 
-print(cat2.eat())
-time.sleep(3)
+print("Все котики сытые и довольные!")
 
-print(cat2.play())
-time.sleep(3)
 
-print(cat2.sleep())
+
+
+
 
